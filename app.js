5fa6e5dd67ca2d0b7d8b61a79f29a62971049d15
Nileshkumar <nileshmaurya250@gmail.com>
2020-05-08 11:26:23 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const cors = require("cors");
+
+const scrape = require("./scrape.js");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({
+    user_Profile_data: req.headers.host + "/api/username",
+    repos: req.headers.host + "/api/username/repos",
+  });
+});
+
+app.get("/api/:username", async (req, res, next) => {
+  const username = req.params.username;
+
+  const userPromise = scrape.getUserData(username);
+  const pinnedPromise = scrape.getPinnedRepo(username);
+  const repoDataPromise = scrape.getEachRepo(username);
+
+  try {
+    const [user, pinnedRepo, repoData] = await Promise.all([
+      userPromise,
+      pinnedPromise,
+      repoDataPromise,
+    ]);
+    res.json({
+      username: user.login,
+      avatar: user.avatar_url,
+      html_url: user.html_url,
+      name: user.name,
+      company: user.company,
+      blog: user.blog,
+      location: user.location,
+      bio: user.bio,
+      repos: user.public_repos,
+      followers: user.followers,
+      following: user.following,
+      created_on: user.created_at,
+      updated_on: user.updated_at,
+      pinnedRepo,
+      repoData,
+    });
+  } catch (err) {
+    res.json({
+      error: err.message,
+    });
+  }
+});
+
+app.get("/api/:username/repos", async (req, res) => {
+  const username = req.params.username;
+  const after = req.query.after;
+  console.log(after, username);
+  const repoData = await scrape.getEachRepo(username, after);
+
+  res.json({
+    repoData,
+  });
+});
+
+const server = app.listen(3030, () => {
+  console.log("server is running on 3030");
+});
---
+++ scrape.js
@@ -0,0 +1,144 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const url = "https://github.com";
+
+function findAndExtractText(element, query) {
+  return element.find(query).text().trim();
+}
+
+module.exports.getUserData = (username) => {
+  return axios
+    .get(`https://api.github.com/users/${username}`)
+    .then((response) => {
+      return response.data;
+    });
+};
+
+module.exports.getPinnedRepo = (username) => {
+  return axios
+    .get(`https://github.com/${username}`)
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+
+      const pinned = [];
+
+      $(".js-pinned-items-reorder-list .pinned-item-list-item-content").each(
+        (_, element) => {
+          const $element = $(element);
+          pinnedRepo = {};
+
+          pinnedRepo.repoName = findAndExtractText($element, "span.repo");
+
+          pinnedRepo.repoLink = url + $element.find("a.text-bold").attr("href");
+
+          pinnedRepo.description = findAndExtractText(
+            $element,
+            "p.pinned-item-desc"
+          );
+
+          pinnedRepo.isForkedFrom = false;
+
+          if ($element.find("p.mb-2").length) {
+            pinnedRepo.isForkedFrom = true;
+            pinnedRepo.ForkedFrom = findAndExtractText($element, "p.mb-2");
+          }
+
+          pinnedRepo.progLang = findAndExtractText(
+            $element,
+            "span[itemprop='programmingLanguage']"
+          );
+
+          pinnedRepo.stargazers = findAndExtractText(
+            $element,
+            `.f6 a[href="/${username}/${pinnedRepo.repoName}/stargazers"]`
+          );
+
+          pinnedRepo.forkCount = findAndExtractText(
+            $element,
+            `.f6 a[href="/${username}/${pinnedRepo.repoName}/network/members"]`
+          );
+
+          pinned.push(pinnedRepo);
+        }
+      );
+
+      return pinned;
+    })
+    .catch((error) => {
+      console.log(error.message);
+      return { error: error.message, message: "User Not Found" };
+    });
+};
+
+module.exports.getEachRepo = (username, next = "") => {
+  return axios
+    .get(`https://github.com/${username}?tab=repositories&after=${next}`)
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+
+      const repos = [];
+
+      $("#user-repositories-list li").each((_, element) => {
+        const $element = $(element);
+        const repo = {};
+
+        repo.repoName = $element.find(".wb-break-all").text().trim();
+
+        repo.repoLink = url + $element.find(".wb-break-all a").attr("href");
+
+        repo.forkedFrom = $element.find("span .muted-link").text();
+
+        if (repo.forkedFrom == "") {
+          repo.isForked = false;
+        } else {
+          repo.isForked = true;
+        }
+        repo.description = $element
+          .find('p[itemprop = "description"]')
+          .text()
+          .trim();
+
+        repo.topics = [];
+
+        $element.find("div.topics-row-container a").each((_, el) => {
+          const $el = $(el);
+          repo.topics.push($el.text().trim());
+        });
+
+        repo.progLang = findAndExtractText(
+          $element,
+          "span[itemprop='programmingLanguage']"
+        );
+
+        repo.stargazers = findAndExtractText(
+          $element,
+          `.f6 a[href="/${username}/${repo.repoName}/stargazers"]`
+        );
+
+        repo.forkCount = findAndExtractText(
+          $element,
+          `.f6 a[href="/${username}/${repo.repoName}/network/members"]`
+        );
+
+        repos.push(repo);
+      });
+
+      const afterValue = $('div[data-test-selector="pagination"] a')
+        .last()
+        .attr("href");
+
+      let after = "";
+
+      if (afterValue) {
+        after = afterValue.match(/after=(.*)/)[1].slice(0, 56);
+        console.log(afterValue, after);
+      }
+
+      return { repos, after };
+    })
+    .catch((error) => {
+      console.log(error.message);
+      return { error: error.message, message: "User Not Found" };
+    });
+};
